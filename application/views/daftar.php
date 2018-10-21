4457ab353dacf29fab6c801cb6bc06c60c0f73d8
Rizky Firmansyah <rizky.firmansyah55@gmail.com>
2018-10-21 11:12:12 +0700

    Login

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -9,48 +9,48 @@
 		  ?>
 
 <div style="margin: 4%">
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="margin-left: 50px">
-      <div class="jumbotron" style="background-color: #FFFFFE">
-      	<center>
-      		<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8" style="margin: 2%">
-      			<img class="img-responsive" src="<?=base_url()?>img/daftarr.png">
-      		</div>  
-      	</center>
-              <h2>Cari Kost mu disini ?</h2><br><br>
-              <p>Forum yang berisi topik tentang Berbagi Ilmu, Ajukan pertanyaanmu dan beri pengguna lain diseluruh Indonesia untuk menjawab pertanyaan mu.</p>
-          </div>
+  <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="margin-left: 50px">
+    <div class="jumbotron" style="background-color: #FFFFFE">
+      <center>
+      	<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8" style="margin: 2%">
+      		<img class="img-responsive" src="<?=base_url()?>img/daftarr.png">
+      	</div>  
+      </center>
+        <h2>Cari Kost mu disini ?</h2><br><br>
+        <p>Forum yang berisi topik tentang Berbagi Ilmu, Ajukan pertanyaanmu dan beri pengguna lain diseluruh Indonesia untuk menjawab pertanyaan mu.</p>
     </div>
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="margin-left: 70px">
-            <?php echo form_open('Kostku/login');?>
-            <?php echo validation_errors(); ?>
-                <div id="login">
+  </div>
+  <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" style="margin-left: 70px">
+    <div id="login">
+      <h2><span class="fontawesome-lock"></span>Sign In</h2>
 
-                    <h2><span class="fontawesome-lock"></span>Sign In</h2>
+      <form id="form_login">
 
-                    <form action="javascript:void(0);" method="POST">
+      <fieldset>
 
-                      <fieldset>
+        <p><label for="uname">Username</label></p>
+        <p><input type="text" class="form-control" name="usernamee" id="uname" onBlur="if(this.value=='')this.value='username'" onFocus="if(this.value=='username')this.value=''"></p> <!-- JS because of IE support; better: placeholder="mail@address.com" -->
 
-                        <p><label for="uname">Username</label></p>
-                        <p><input type="text" class="form-control" name="usernamee" id="uname" value="username" onBlur="if(this.value=='')this.value='username'" onFocus="if(this.value=='username')this.value=''"></p> <!-- JS because of IE support; better: placeholder="mail@address.com" -->
+        <p><label for="paswd">Password</label></p>
+        <p><input type="password"  class="form-control" name="paswd" id="paswd" onBlur="if(this.value=='')this.value='password'" onFocus="if(this.value=='password')this.value=''"></p> <!-- JS because of IE support; better: placeholder="password" -->
 
-                        <p><label for="paswd">Password</label></p>
-                        <p><input type="password"  class="form-control" name="paswd" id="paswd" value="password" onBlur="if(this.value=='')this.value='password'" onFocus="if(this.value=='password')this.value=''"></p> <!-- JS because of IE support; better: placeholder="password" -->
+        <div id="responseDiv" class="alert text-center" style="margin-top:20px; display:none;">
+          <button type="button" class="close" id="clearMsg"><span aria-hidden="true">&times;</span></button>
+          <span id="message"></span>
+        </div>
 
-                        <p><input type="submit" value="Sign In"></p>
-                          <div style="text-align: center;">
-                              <span align="center">Not Registered? <a href="" style="text-decoration: none" data-toggle="modal" data-target="#myModal">Create an account</a></span>
-                          </div>
-                      </fieldset>
+        <center><button type="button" class="btn btn-success" id="btn_login">Sign In</button></center>
+        <div style="text-align: center;">
+          <span align="center">Not Registered? <a href="" style="text-decoration: none" data-toggle="modal" data-target="#myModal">Create an account</a></span>
+        </div>
+      </fieldset>
 
-                    </form>
+      </form>
                     
                     
 
-                  </div>        
-              <?php echo form_close(); ?> 
-  
     </div>
+  </div>
 </div>
 
 <!-- Modal -->
@@ -64,34 +64,36 @@
         <h4 class="modal-title">Daftar Sekarang</h4>
       </div>
       <div class="modal-body">
-        <?php echo form_open('Kostku/mendaftar');?>
-        <?php echo validation_errors(); ?>
         
-              <div class="form-group">
-                <label for="usr">Name :</label>
-                <input type="text" class="form-control" name="usr" placeholder="username" required="not null">
-              </div>
-              <div class="form-group">
-                <label for="usr">Nomor :</label>
-                <input type="text" class="form-control" name="nohp" placeholder="Nomor Hp" required="not null">
-              </div>
-              <div class="form-group">
-                <label for="email">Email :</label>
-                <input type="email" class="form-control" name="email" placeholder="email@address.com" required="">
-              </div>
-              <div class="form-group">
-                <label for="pwd">Password :</label>
-                <input type="password" class="form-control" name="pwd" id="pwd"  required="">
-              </div>              
-              <div class="form-group">
-                <label for="pwd">Re-Password :</label>
-                <input type="password" class="form-control" name="repwd" required="">
-              </div>
+        <div class="form-group">
+          <label for="usr">Name :</label>
+          <input type="text" class="form-control" name="usr" id="user_sign" placeholder="username" required="not null">
+        </div>
+        <div class="form-group">
+          <label for="usr">Nomor :</label>
+          <input type="text" class="form-control" name="nohp" id="no_hp_sign" placeholder="Nomor Hp" required="not null">
+        </div>
+        <div class="form-group">
+          <label for="email">Email :</label>
+          <input type="email" class="form-control" name="email" id="email_sign" placeholder="email@address.com" required="">
+        </div>
+        <div class="form-group">
+          <label for="pwd">Password :</label>
+          <input type="password" class="form-control" name="pwd" id="pass_sign"  required="">
+        </div>              
+        <div class="form-group">
+          <label for="pwd">Re-Password :</label>
+          <input type="password" class="form-control" name="repwd" id="con_pass_sign" required="">
+        </div>
+
+        <div id="responseDivS" class="alert text-center" style="margin-top:20px; display:none;">
+          <button type="button" class="close" id="clearMsgS"><span aria-hidden="true">&times;</span></button>
+          <span id="messageS"></span>
+        </div>
             
-            <CENTER>
-                <input type="submit" class="btn" name="signup" value="Daftar" style="background-color: #27AB27;color: #fff">
-            </CENTER>
-        <?php echo form_close(); ?> 
+        <CENTER>
+          <button type="button" class="btn btn-success" id="btn_sign">Sign Up</button>
+        </CENTER>
       </div>
       <div class="modal-footer">
        
--- application/views/header_footer/footer.php
+++ application/views/header_footer/footer.php
@@ -8,175 +8,88 @@
 		<script type="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 
 		<script type="text/javascript">
+	    $(document).ready(function(){
 
-        $(document).ready(function(){
-        	pertanyaan();
+			$('#btn_login').on('click',function(){
+	            var username = $('#uname').val();
+	            var password = $('#paswd').val();
+	            $.ajax({
+	                type : "POST",
+	                url  : "<?php echo site_url('Login/login') ?>",
+	                dataType : "JSON",
+	                data : {username:username,password:password},
+	                success: function(response){
+	                	$('#message').html(response.message);
+						if(response.error){
+							$('#responseDiv').removeClass('alert-success').addClass('alert-danger').show();
+						}
+						else{
+							$('#responseDiv').removeClass('alert-danger').addClass('alert-success').show();
+							$('#uname').val("");
+							$('#paswd').val("");
+							if (response.level == "Administrator") {
+								setTimeout(' window.location.href = "<?php echo site_url('Admin'); ?>" ',3000);
+							}else{
+								setTimeout(' window.location.href = "<?php echo site_url('User'); ?>" ',3000);
+							}
+						}
+	                }
+	            });
 
-        		$("#Bertanya").click(function(){
-        			pertanyaan();
-		        });
-		        
-		        $("#Menjawab").click(function(){
-        			menjawab();
-		        });
+	            $(document).on('click', '#clearMsg', function(){
+					$('#responseDiv').hide();
+				});
 
-		        $("#liked").click(function(){
-        			like();
-		        });
+	            return false;
+	        });
 
-		        function like() {
-		        	var id_quest = $('#questionjawab').val();
-		        	var id_jwb = $('#id_jawab').val();
-		        	 $.ajax({
+	        $('#btn_sign').on('click',function(){
+	            var username = $('#user_sign').val();
+	            var email = $('#email_sign').val();
+	            var no_hp = $('#no_hp_sign').val();
+	            var password = $('#pass_sign').val();
+	            var confirm_password = $('#con_pass_sign').val();
+	            if (confirm_password != password) {
+	            	$('#messageS').html('Your Passwords Must Match');
+	            	$('#responseDivS').removeClass('alert-success').addClass('alert-danger').show();
+	            }
+	            else{
+	            	$.ajax({
 		                type : "POST",
-		                url  : "<?php echo site_url('Helpmy/like'); ?>",
+		                url  : "<?php echo site_url('Login/sign_up') ?>",
 		                dataType : "JSON",
-		                data : {id_q:id_quest,id_j:id_jwb},
-		                success: function(){
-		                    // $('[name="id_manga_recomend"]').val("");
-		                    // $('#Modal_Recomend').modal('hide');
-		                    // $(".table").DataTable().destroy();
-		                    // $('tbody').empty();	
-		                    // show_manga();
+		                data : {username:username,email:email,no_hp:no_hp,password:password},
+		                success: function(response){
+		                	$('#messageS').html(response.message);
+							$('#sigText').html('Sign Up');
+							if(response.error){
+								$('#responseDivS').removeClass('alert-success').addClass('alert-danger').show();
+							}
+							else{
+								$('#responseDivS').removeClass('alert-danger').addClass('alert-success').show();
+								$('#user_sign').val("");
+								$('#email_sign').val("");
+								$('#no_hp_sign').val("");
+								$('#pass_sign').val("");
+								$('#con_pass_sign').val("");
+								// if (response.level == "Administrator") {
+								// 	setTimeout(' window.location.href = "<?php echo site_url('Admin'); ?>" ',3000);
+								// }
+							}
 		                }
 		            });
-		            return false;
-		        }
+	            }
 
-                $("#btn_save").click(function(){
-                	 if (true) {
-                		$('#id_q_t').attr('value',$('#id_q').val());
-                		$('#id_hadiah_t').attr('value',$('#id_hadiah').val());
-                		$('#id_poin_t').attr('value',$('#id_poin').val());
-			            $('#jwb_t').text($('#jwb').val());
-			            $('#modal_jwb').modal('show');
-                	}
-		            
-		        });
+	            $(document).on('click', '#clearMsgS', function(){
+					$('#responseDivS').hide();
+				});
 
-		        $("#answer_Edit").click(function(){
-                	 if (true) {
-                		$('#id_a_t').attr('value',$('#id_comt').val());
-			            $('#editQusetion').modal('show');
-                	}
-		            
-		        });
-		        
-		        $(".delete_comnt").click(function(){
-                	
-		        	return confirm("Apa anda YAKIN ingin HAPUS ini ?");
+	            return false;
+	        });
+	 
+	    });
+	     
+	</script>
 
-		        });
-
-		        function pertanyaan(){
-                	 $.ajax({
-			                type  : 'ajax',
-			                url   : '<?php echo site_url('Helpmy/showPertanyaan')?>',
-			                async : false,
-			                dataType : 'json',
-			                success : function(data){
-			                    var html = '';
-			                    var i;
-			                    for(i=0; i<data.length; i++){
-			                    	 html += 
-			                    	 		'<div style="background-color: #FFFFFF">'+
-												'<br>'+
-												'<div style="padding-left: 4%;padding-right: 4%;padding-top: 1%;">'+
-													'<div class="row">'+
-														'<img src="<?=base_url()?>img/upload/'+data[i].foto+'" width="32" height="32">&nbsp'+
-														'<div style="margin-left: 2%">'+
-															'<b>'+data[i].id_user+'</b> <br>'+
-																data[i].tanggal+
-														'</div>'+
-													'</div>'+
-													
-													'<div style="padding-left: 5%;padding-right: 1%;padding-top: 0.5%;font-size: 18px;">'+
-														'<a style="color: black" href="<?=site_url()?>/Helpmy/detailpertanyaan/'+data[i].id_q+'">'+	
-															data[i].pertanyaan+
-														'</a>'+
-													'</div>'+
-													'<br>'+
-													'<div style="padding-left: 5%">'+
-														'<button type="button" class="btn btn-default btn-xs button_like"><b>❤ TERIMA KASIH </b></button> &nbsp'+
-														'<a href="">Komentar (0)</a> &nbsp'+
-													'</div>'+
-													'<br>'+
-													'<div style="height: 1px;background-color: #E1E8ED"></div>'+
-												'</div>'+
-											'</div>';
-			                    }
-			                    $('#pertanyaan').html(html);
-			                }
-			            });
-		        }
-
-		        function menjawab(){
-                	 $.ajax({
-			                type  : 'ajax',
-			                url   : '<?php echo site_url('Helpmy/showMenjawab')?>',
-			                async : false,
-			                dataType : 'json',
-			                success : function(data){
-			                    var html = '';
-			                    var i;
-
-			                    for(i=0; i<data.length; i++){
-			                    	var fotot = $('#foto'+data[i].id_penanya).val();			                    	
-			                    	var fotoj = $('#foto'+data[i].id_pnjwb).val();
-			                    	 html += 
-			                    	 		'<div style="background-color: #FFFFFF">'+
-												'<br>'+
-												'<div style="padding-left: 4%;padding-right: 4%;padding-top: 1%;">'+
-													'<div class="row">'+	
-														'<img src="<?=base_url()?>img/upload/'+fotot+'" width="32" height="32">&nbsp '+
-														'<div style="margin-left: 2%">'+
-															'<b>'+data[i].id_penanya+'</b><br>'+
-															'<div style="font-size: 11px">'+
-																data[i].kategori+
-															'</div>'+
-														'</div>'+
-													'</div>'+
-													
-													'<div style="padding-left: 5%;padding-right: 1%;padding-top: 0.5%;font-size: 18px;">'+
-														'<a style="color: black" href="<?=site_url()?>/Helpmy/detailpertanyaan/'+data[i].id_question+'">'+
-															data[i].pertanyaan+
-														'</a>'+
-														'<div style="height: 1px;background-color: #207245"></div>'+
-													'</div>'+
-													'<br>'+
-													'<div style="padding-left: 5%">'+
-														'<!-- sUBBBBBBBBBBBBb -->'+
-														'<div style="padding-left: 4%;padding-right: 4%;padding-top: 1%;">'+
-															'<div class="row">'+
-																'<img src="<?=base_url()?>img/upload/'+fotoj+'" width="32" height="32">&nbsp '+
-																'<div style="margin-left: 2%;font-size: 12px">'+
-																	// '<b>'data[i].id_pnjwb+'</b> <br>'+
-																	data[i].tanggal+
-																'</div>'+
-															'</div>'+
-															
-															'<div style="padding-left: 7%;padding-right: 1%;padding-top: 0.5%;font-size: 18px;">'+
-																data[i].jawaban+
-																'<div style="height: 1px;background-color: #207245"></div>'+
-															'</div>'+
-														'</div>'+
-														 '&nbsp'+
-													'</div>'+
-													'<br>'+
-													'<div style="height: 1px;background-color: #E1E8ED"></div>'+
-												'</div>'+
-											'</div>';
-			                    }
-			                    $('#pertanyaan').html(html);
-			                }
-			            });
-		        }
-
-		       
-
-
-        });
-
-    </script>
 	</body>
 </html>
