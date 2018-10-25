c730260cfe9d5828a9b6b9703c55d9bca1985a5c
wildanal2PC <40136374+wildanal2@users.noreply.github.com>
2018-10-25 21:24:24 +0700

    user fix v3
    user model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -10,8 +10,9 @@ class User_model extends CI_Model {
        	return $query->result();
 	}	
 
-	public function save_kost($alamat,$Kelamin,$deskripsi,$foto_kost,$id_pemilik){
+	public function save_kost($nama,$alamat,$Kelamin,$deskripsi,$foto_kost,$id_pemilik){
         $data = array(
+                'nama'  => $nama,
                 'alamat'  => $alamat, 
                 'kota'  => "Malang", 
                 'propinsi' => "Jawa Timur", 
@@ -23,10 +24,11 @@ class User_model extends CI_Model {
             );
         $result=$this->db->insert('kost',$data);
         return $result;
-    }	
+    }
 
-    public function edit_kost($id_kost,$alamat,$Kelamin,$deskripsi,$foto_kost){
+    public function edit_kost($id_kost,$nama,$alamat,$Kelamin,$deskripsi,$foto_kost){
         $data = array(
+                'nama'  => $nama,
                 'alamat'  => $alamat,
                 'Kelamin' => $Kelamin,
                 'deskripsi' => $deskripsi,
@@ -43,6 +45,63 @@ class User_model extends CI_Model {
 		$result = $this->db->delete('kost');
 		return $result;
 	}
+
+    public function getKamar($id_kost)
+    {
+        $this->db->where('id_kost', $id_kost);
+        $query=$this->db->get('kamar');
+        return $query->result();
+    }
+
+    public function save_kamar($ukuran,$foto_kamar,$harga,$id_kost){
+        $data = array(
+                'ukuran'  => $ukuran,
+                'foto_kamar'  => $foto_kamar, 
+                'harga' => $harga,
+                'is_rented' => 0,
+                'id_kost' => $id_kost
+            );
+        $result=$this->db->insert('kamar',$data);
+        return $result;
+    }
+
+    public function edit_kamar($id_kamar,$ukuran,$harga,$foto_kamar){
+        $data = array(
+                'ukuran'  => $ukuran,
+                'foto_kamar'  => $foto_kamar,
+                'harga' => $harga
+            );
+        $this->db->where('id_kamar', $id_kamar);
+        $result=$this->db->update('kamar',$data);
+        return $result;
+    }
+
+    public function delete_kamar($id_kamar)
+    {
+        $this->db->where('id_kamar', $id_kamar);
+        $result = $this->db->delete('kamar');
+        return $result;
+    }
+
+    public function sewakan($id_kamar)
+    {
+        $data = array(
+                'is_rented' => 1
+            );
+        $this->db->where('id_kamar', $id_kamar);
+        $result=$this->db->update('kamar',$data);
+        return $result;
+    }
+
+    public function kosongkan($id_kamar)
+    {
+        $data = array(
+                'is_rented' => 0
+            );
+        $this->db->where('id_kamar', $id_kamar);
+        $result=$this->db->update('kamar',$data);
+        return $result;
+    }
 
 }
 
